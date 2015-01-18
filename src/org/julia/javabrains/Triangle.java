36dd7688a7fdb3da9252af42011847f9c7af944d
Bdgolka <juliapulova@yandex.ru>
2015-01-18 09:51:43 +0200

    Spring Tutorial 06
    Using Constructor Injection

--- src/org/julia/javabrains/Triangle.java
+++ src/org/julia/javabrains/Triangle.java
@@ -3,15 +3,35 @@ package org.julia.javabrains;
 public class Triangle {
 
 	private String type;
+	private int height;
+
+	public Triangle(String type) {
+		this.type = type;
+	}
 	
+	public Triangle(int height) {
+		this.height = height;
+	}
+	
+	
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
+
 	public String getType() {
 		return type;
 	}
+
+	public int getHeight() {
+		return height;
+	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
-	public void draw(){
-		System.out.println(getType() + " Triangle drawn");
+
+	public void draw() {
+		System.out.println(getType() + " Triangle drawn of height " + getHeight());
 	}
 }
-
