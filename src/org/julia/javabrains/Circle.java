3a3c6bb9524a6bb3e78d13e0a82623286aa2ad70
Julia Pulova <juliapulova@yandex.ru>
2015-01-21 15:32:14 +0200

    Spring Tutorial 21
    Component and Stereotype Annotations

--- src/org/julia/javabrains/Circle.java
+++ src/org/julia/javabrains/Circle.java
@@ -4,6 +4,15 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.annotation.Resource;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+//@Component //declaring class as a bean and no need to declare it in spring.xml
+//@Service
+//@Repository
+@Controller
 public class Circle implements Shape {
 
 	protected Point center;
@@ -28,11 +37,10 @@ public class Circle implements Shape {
 	public void intializeXircle() {
 		System.out.println("Init of circle");
 	}
-	
+
 	@PreDestroy
 	public void destroyXircle() {
 		System.out.println("Destroy of circle");
 	}
-
 
 }
